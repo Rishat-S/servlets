7ddbd0824a79c8bd93584f8ef35830a45c22c7dc
Rishat Suleymanov <spk_irbis@mail.ru>
2021-07-07 00:49:33 +0300

    Refactoring

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,28 +2,30 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 // Stub
 public class PostRepository {
+    private long id;
+    private final Map<Long, Post> posts = new HashMap<>();
+
     public List<Post> all() {
-        return Collections.emptyList();
+        return new ArrayList<>(posts.values());
     }
 
     public Optional<Post> getById(long id) {
-        return Optional.empty();
+        return Optional.of(posts.get(id));
     }
 
-    public Post save(Post post) {
-        return post;
+    public synchronized Post save(Post post) {
+        return posts.put(++id, post);
     }
 
-    public void removeById(long id) {
+    public synchronized Post update(Post post) {
+        return posts.put(post.getId(), post);
     }
 
-    public Post update(Post post) {
-        return post;
+    public synchronized void removeById(long id) {
+        posts.remove(id);
     }
 }

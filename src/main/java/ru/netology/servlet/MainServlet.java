bcb6975796e22b5acaa676ee29a2eebe67912617
Rishat Suleymanov <spk_irbis@mail.ru>
2021-07-10 00:16:21 +0300

    Implementing dependency injection using Java configuration

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
@@ -18,9 +20,10 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        final var repository = context.getBean("postRepository", PostRepository.class);
+        final var service = context.getBean("postService", PostService.class);
+        controller = context.getBean("postController", PostController.class);
     }
 
     @Override
